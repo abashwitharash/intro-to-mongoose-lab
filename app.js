135ebc09403684b67e79f21f0d32c846fd09c34b
Arash <arashsaleh798@gmail.com>
2025-02-13 15:19:50 -0800

    lab complete

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-const prompt = require('prompt-sync')();
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const Customer = require('./models/customer')
 
+// const prompt = require('prompt-sync')();
 
-const username = prompt('What is your name? ');
+// const welcome = prompt('Welcome to the CRM\n\nWhat would you like to do?\n\n1. Create a customer\n2. View all customers\n3. Update a customer\n4. Delete a customer\n5. quit')
 
-console.log(`Your name is ${username}`);
+// console.log(welcome);
+
+// const welcome = prompt('What is your name? ');
+
+// console.log(`Your name is ${username}`);
+
 
 const connect = async () => {
     // Connect to MongoDB using the MONGODB_URI specified in our .env file.
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
-};
+
+    await runQueries()
+
+    await mongoose.disconnect();
+  console.log('Disconnected from MongoDB');
+
+  process.exit();
+
+}
+
+const runQueries = async () => {
+    console.log('Queries running.')
+    // The functions calls to run queries in our db will go here as we write them.
+      await findCustomers();
+    //   await createCustomer();
+  };
+  
+  connect()
+
+
+  const createCustomer = async () => {
+    const customerData = {
+        name: 'Mica',
+        age: 15,
+    }
+
+    const customer = await Customer.create(customerData) // how data is created on MongoDB
+    console.log("new customer:", customer);
+}
+
+const findCustomers = async () => {
+    const customers = await Customer.find({})
+    console.log('All customers:', customers);
+}
--- models/customer.js
+++ models/customer.js
@@ -1,15 +1,14 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const mongoose = require('mongoose');
-const Customer = require('./models/customer')
+
 
 const customerSchema = new mongoose.Schema({
     name: String,
     age: Number,
   });
 
-  const connect = async () => {
-    // Connect to MongoDB using the MONGODB_URI specified in our .env file.
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to MongoDB');
-};
+//define and export customer model 
+const Customer = mongoose.model('Customer', customerSchema );
+// execute Customer
+module.exports = Customer;
+
+
