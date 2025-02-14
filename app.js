da67808b00af57405c91baa4a80aa5a5f830ef8b
Arash <arashsaleh798@gmail.com>
2025-02-13 17:12:12 -0800

    lab complete again

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const Customer = require('./models/customer')
 
 // console.log(`Your name is ${username}`);
 
-
+//STEP ONE CONNECT TO MONGO AND THEN MAKE THE START FUNCTIONS
 const connect = async () => {
     // Connect to MongoDB using the MONGODB_URI specified in our .env file.
     await mongoose.connect(process.env.MONGODB_URI);
@@ -27,28 +27,34 @@ const connect = async () => {
   process.exit();
 
 }
-
+//STEP 2 HAVE A RUN QUERIES TO RUN THE FUNCTIONS AND WHEN MADE AWAIT AND RUN FUNCTIONS HERE
 const runQueries = async () => {
     console.log('Queries running.')
     // The functions calls to run queries in our db will go here as we write them.
       await findCustomers();
-      await createCustomer();
+      // await createCustomer();
   };
   
-  connect()
 
+ 
 
+//STEP 3 CREATE AND MAKE FUNCTIONS 
   const createCustomer = async () => {
     const customerData = {
-        name: 'Arash',
-        age: 35,
-    }
-
+        name: 'Arash', age: 35,
+        name: 'Mica', age: 10,
+    };
+//making a constand and calling it customer = teling it to wait to understand the info 
+// and then grabbing the schematics (Customer) and create it with the customerData aka my name and age 
     const customer = await Customer.create(customerData) // how data is created on MongoDB
     console.log("new customer:", customer);
-}
+};
 
 const findCustomers = async () => {
     const customers = await Customer.find({})
     console.log('All customers:', customers);
-}
+};
+//STEP 4 RUN PROGRAM
+connect();
+
+
--- models/customer.js
+++ models/customer.js
@@ -1,11 +1,14 @@
+//OFFICIALLY START HERE
+//STEP ONE
 const mongoose = require('mongoose');
 
-
+// STEP 2
 const customerSchema = new mongoose.Schema({
     name: String,
     age: Number,
   });
 
+  // STEP 3
 //define and export customer model 
 const Customer = mongoose.model('Customer', customerSchema );
 // execute Customer
